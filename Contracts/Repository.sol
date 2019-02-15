28cbd4238829aa783c1bb975ece0052d6db0cca8
Manuel <manuel.keller@bf.uzh.ch>
2019-02-15 10:25:29 +0100

    update and event

--- Contracts/Repository.sol
+++ Contracts/Repository.sol
@@ -14,6 +14,13 @@ contract Repository {
 
   VNF[] VNF_repository;
 
+  event License(
+        address buyer,
+        uint index,
+        string repository_link,
+        bytes32 repository_hash
+    );
+
   function register_VNF(string memory name, string memory description, string memory service_type,
                         string memory repository_link, uint price, string memory version,
                         bytes32 repository_hash) public {
@@ -25,21 +32,32 @@ contract Repository {
     VNF_repository.push(vnf);
   }
 
+  function update_VNF(uint index, string memory name, string memory description, string memory service_type,
+                        string memory repository_link, uint price, string memory version,
+                        bytes32 repository_hash) public {
+    require(msg.sender == VNF_repository[index].author, "Only author can update VNF package");
+    VNF_repository[index].name = name;
+    VNF_repository[index].description = description;
+    VNF_repository[index].service_type = service_type;
+    VNF_repository[index].repository_link = repository_link;
+    VNF_repository[index].price = price;
+    VNF_repository[index].version = version;
+    VNF_repository[index].repository_hash = repository_hash;
+  }
+
   function retrieve_numberOf_vnf() public view returns (uint count) {
     count = VNF_repository.length;
   }
 
   function retrieve_vnf(uint index) public view returns
-  (string memory name, string memory description, string memory service_type, string memory repository_link,
-   uint price, address author, string memory version, bytes32 repository_hash) {
+  (string memory name, string memory description, string memory service_type,
+   uint price, address author, string memory version) {
      name = VNF_repository[index].name;
      description = VNF_repository[index].description;
      service_type = VNF_repository[index].service_type;
-     repository_link = VNF_repository[index].repository_link;
      price = VNF_repository[index].price;
      author = VNF_repository[index].author;
      version = VNF_repository[index].version;
-     repository_hash = VNF_repository[index].repository_hash;
   }
 
   function delete_VNF(uint index) public {
@@ -53,5 +71,7 @@ contract Repository {
     address payable receiver = VNF_repository[index].author;
     uint amount = VNF_repository[index].price;
     receiver.transfer(amount);
+
+    emit License(msg.sender, index, VNF_repository[index].repository_link, VNF_repository[index].repository_hash);
   }
 }
